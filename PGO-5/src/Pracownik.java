fddcc2885c663843d938b273afb711b1406b67bb
Bazyl Nowicki <s36080@pjwstk.edu.pl>
2026-06-12 23:53:43 +0200

    PGO-5: dziedziczenie zespol projektowy

---
+++ PGO-5/src/Pracownik.java
@@ -0,0 +1,53 @@
+import java.util.Objects;
+
+public class Pracownik {
+
+    private String idPracownika;
+    private String imie;
+    private String nazwisko;
+    private double stawkaBazowa;
+
+    public Pracownik(String idPracownika, String imie, String nazwisko, double stawkaBazowa) {
+        this.idPracownika = idPracownika;
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.stawkaBazowa = stawkaBazowa;
+    }
+
+    public final String pobierzIdPracownika() {
+        return idPracownika;
+    }
+
+    public String getImie() { return imie; }
+    public String getNazwisko() { return nazwisko; }
+    public double getStawkaBazowa() { return stawkaBazowa; }
+
+    public double obliczKosztMiesieczny() {
+        return stawkaBazowa;
+    }
+
+    public String przedstawSie() {
+        return "Pracownik: " + imie + " " + nazwisko + " [" + idPracownika + "]";
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "{id='" + idPracownika + "'" +
+                ", imie='" + imie + "'" +
+                ", nazwisko='" + nazwisko + "'" +
+                ", kosztMiesieczny=" + obliczKosztMiesieczny() + "}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pracownik)) return false;
+        Pracownik other = (Pracownik) o;
+        return Objects.equals(this.idPracownika, other.idPracownika);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idPracownika);
+    }
+}
